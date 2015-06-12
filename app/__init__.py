14f97402d99b3af6cae7f91317667732c2447579
princeoflight <nsmichael31@gmail.com>
2015-06-12 13:56:03 -0500

    add logging

--- app/__init__.py
+++ app/__init__.py
@@ -3,4 +3,12 @@ from flask import Flask
 app = Flask(__name__)
 app.config.from_object('config')
 
+if app.debug is not True:   
+    import logging
+    from logging.handlers import RotatingFileHandler
+    file_handler = RotatingFileHandler('python.log', maxBytes=1024 * 1024 * 100, backupCount=20)
+    file_handler.setLevel(logging.ERROR)
+    app.logger.setLevel(logging.ERROR)
+    app.logger.addHandler(file_handler)
+
 from app import views

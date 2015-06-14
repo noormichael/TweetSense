686825cead78a01543eb4d4c2d9736835d41fbb8
princeoflight <nsmichael31@gmail.com>
2015-06-14 10:04:09 -0500

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -3,8 +3,4 @@ from flask import Flask
 app = Flask(__name__)
 app.config.from_object('config')
 
-import logging
-app.logger.addHandler(logging.StreamHandler(sys.stdout))
-app.logger.setLevel(logging.ERROR)
-
 from app import views

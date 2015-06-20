527c6b519e99eef9318689c3226270dc55f9cb74
princeoflight <nsmichael31@gmail.com>
2015-06-19 23:09:43 -0500

    Update views.py

--- app/views.py
+++ app/views.py
@@ -90,6 +90,8 @@ def results():
   query = session['qu']
   if not query2 == "":
     query2 = session['qu2']
+    
+  return result = "\n".join("\t".join(map(str,l)) for l in d)
   
   q1Invalid = False
   q2Invalid = False

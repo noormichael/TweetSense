97bc50f83ebe3e5bf5da7527f649d47fa1745178
princeoflight <nsmichael31@gmail.com>
2015-06-22 01:19:31 -0500

    Update views.py

--- app/views.py
+++ app/views.py
@@ -81,6 +81,9 @@ def results():
     session['qu2'] = ""
     session['qu'] = str(form.query.data.replace('#','').strip())
     session['qu2'] = str(form.opQuery.data.replace('#','').strip())
+    
+    return session['qu']
+    
     if session['qu'] == "":
       return redirect('/index')
     q1Invalid, q2Invalid = setGraphs(form, session['qu'], session['qu2'])
